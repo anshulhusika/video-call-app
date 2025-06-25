b1935824f925c59faf92c59dbadbf83a28c1f09a
Anshul Husika <anshulhusika@gmail.com>
2025-06-25 22:29:47 +0530

    next day test with excel wednesday -back camera - drag 5

--- src/App.jsx
+++ src/App.jsx
@@ -152,7 +152,7 @@ const App = () => {
         muted
         playsInline
         className="local-video"
-        style={{ transform: 'scaleX(-1)' }}
+        
         initial={{ opacity: 0, scale: 0.9 }}
         animate={{ opacity: 1, scale: 1 }}
         transition={{ duration: 0.5 }}
